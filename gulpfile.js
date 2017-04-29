297e7f06082fc5268bb605f7a8b7062feb60b344
Ruoni Wang <Ruoni.wang.19@Dartmouth.edu>
2017-04-29 13:23:30 -0400

    ruoni changes

--- gulpfile.js
+++ gulpfile.js
@@ -7,11 +7,32 @@
 const gulp = require('gulp');
 const pug = require('gulp-pug');
 const browserSync = require('browser-sync').create();
+const sass = require('gulp-sass');
+const coffeescript = require('gulp-coffeescript');
+
+gulp.task('coffee', function() {
+  gulp.src('./src/*.coffee')
+    .pipe(coffeescript({bare: true}).on('error', gutil.log))
+    .pipe(gulp.dest('./public/js')).
+    .pipe(browserSync.reload({
+      stream: true,
+    }));
+});
+
 
 gulp.task('compile-pug', () => {
   gulp.src('./src/pug/*.pug') // path to your file
     .pipe(pug())
     .pipe(gulp.dest('./public/html/'))
+    .pipe(browserSync.reload({
+      stream: true,
+    }));
+});
+
+gulp.task('sass', () => {
+  return gulp.src('./src/style/*.scss')
+    .pipe(sass()) // Using gulp-sass
+    .pipe(gulp.dest('./public/style'))
     .pipe(browserSync.reload({
       stream: true,
     }));
