1bcced8da5248a2b8632c675af87ad6fe4ae085c
Ruoni Wang <Ruoni.wang.19@Dartmouth.edu>
2017-04-29 16:05:30 -0400

    minify

--- gulpfile.js
+++ gulpfile.js
@@ -14,10 +14,13 @@ const uglify = require('gulp-uglify');
 const gulpIf = require('gulp-if');
 const cssnano = require('gulp-cssnano');
 const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const rename = require('gulp-rename');
 
 gulp.task('coffee', function() {
   gulp.src('./src/*.coffee')
     .pipe(coffeescript())
+    .pipe(uglify())
     .pipe(gulp.dest('./public'))
     .pipe(browserSync.reload({
       stream: true,
@@ -36,6 +39,7 @@ gulp.task('compile-pug', () => {
 gulp.task('sass', () => {
   return gulp.src('./src/style/*.scss')
     .pipe(sass()) // Using gulp-sass
+    .pipe(cssnano())
     .pipe(gulp.dest('./public'))
     .pipe(browserSync.reload({
       stream: true,
@@ -57,18 +61,15 @@ gulp.task('browserSync', () => {
   });
 });
 
-gulp.task('useref', () => {
-  return gulp.src('public')
-    .pipe(useref())
-    .pipe(gulpIf('*.js', uglify()))
-    .pipe(gulpIf('*.css', cssnano()))
-    .pipe(gulp.dest('public/dist'));
+gulp.task('images', () => {
+  return gulp.src('src/image/*.+(png|jpg|gif|svg)')
+  .pipe(imagemin())
+  .pipe(gulp.dest('public/image'));
 });
 
-gulp.task('images', () => {
-  return gulp.src('src/images/**/*.+(png|jpg|gif|svg)')
-  .pipe(imagemin())
-  .pipe(gulp.dest('dist/images'));
+
+gulp.task('default', ['watch'], function () {
+    // This will only run if the lint task is successful...
 });
 
 // pug
